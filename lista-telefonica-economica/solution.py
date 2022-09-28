e61e7fb605e1fc33baf1546fdafbcab00c999146
Danilo Borges da Silva <daniloborges.cg@gmail.com>
2022-09-28 00:09:57 -0300

    lista telefonica

--- lista-telefonica-economica/solution.py
+++ lista-telefonica-economica/solution.py
@@ -3,18 +3,21 @@ while True:
         n = int(input())
         list_values = []
         for values in range(n):
-            list_values.append(int(input()))
+            list_values.append(input())
         list_values.sort()
         economy = 0
-        
+        # print(list_values)
         for entry in range(n-1):
             number = str(list_values[entry])
             number_compare = str(list_values[entry+1])
             #verificar substrig máxima inicial
-            for i in range(len(number)-1):
-                if not number[:i+1] == number_compare[:i+1]:
-                    economy += i
-                    break
+            if number == number_compare:
+                economy += len(number)
+            else:
+                for i in range(len(number)):
+                    if not number[:i+1] == number_compare[:i+1]:
+                        economy += i
+                        break
         print(economy)        
     except EOFError:
         break
